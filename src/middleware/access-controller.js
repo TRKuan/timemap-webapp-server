4eddb3adc6b84961611f6959a87a95053a39d14f
104060011 <twtnyk@gmail.com>
2017-06-17 00:49:16 +0800

    magical init

--- src/middleware/access-controller.js
+++ src/middleware/access-controller.js
@@ -1,17 +1,6 @@
-//老實說我也不知道這幹嘛用的，反正老師的有我就加進來了
-//算了先註解掉，要用在打開
-/*
-這裡是本來的程式碼
+
 const fs = require('fs');
-*/
 
-/*
- * Grants access for cross-origin HTTP requests.
- * See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
- */
-
-/*
-這裡是本來的程式碼
 module.exports = function(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     if (req.get('Access-Control-Request-Headers'))
@@ -21,4 +10,3 @@ module.exports = function(req, res, next) {
 
     next();
 };
-*/
